1f21b1325e1615e8b7d9c1025c072c5cc2f40535
Nitya Verma <vrmnitya@gmail.com>
2023-12-29 18:39:36 +0530

    create the layout

--- app.py
+++ app.py
@@ -1,2 +1,31 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+
+st.set_page_config(
+    page_title="Product Review Analysis",
+    page_icon="💠",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+
+st.sidebar.title('Product Review Analysis')
+option = st.sidebar.selectbox('Select one', ['Analysis', 'Input Review'])
+if option == "Analysis":
+    with st.sidebar:
+        add_radio = st.radio(
+            "Choose analysis",
+            ("EDA", "Sentiment Analysis")
+        )
+    if add_radio == 'EDA':
+        st.title('EDA')
+    else:
+        st.title('Sentiment Analysis')
+
+else:
+    st.title('Input Review')
+
