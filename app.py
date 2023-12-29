13bbbe85f3f1258ae70e7e955ea6201b91751046
Nitya Verma <vrmnitya@gmail.com>
2023-12-30 00:22:52 +0530

    Add word cloud

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-df = pd.read_csv('reviews_cleaned.csv')
+
+df = pd.read_csv('rev_cleaned.csv')
 
 st.set_page_config(
     page_title="Product Review Analysis",
@@ -19,6 +23,27 @@ def eda():
     avg_rating = df['Score'].mean()     #finds the average rating
     st.metric(label= 'Average Rating',value= avg_rating)
 
+    # for positive sentiment
+    positive_reviews = df[df["sc"] == 'Positive']['Text'].sample(n=1000, random_state=42).str.cat(sep=' ')
+    st.subheader('Word Cloud For postive reviews')
+    plt.figure(figsize=(20, 20))
+    wc = WordCloud(width=1600, height=800).generate(positive_reviews)
+    plt.imshow(wc)
+    st.pyplot()
+
+    negative_reviews = df[df["sc"] == 'Negative']['Text'].sample(n=1000, random_state=42).str.cat(sep=' ')
+    st.subheader('Word Cloud For negative reviews')
+
+    plt.figure(figsize=(20, 20))
+    wc = WordCloud(width=1600, height=800).generate(negative_reviews)
+    plt.imshow(wc)
+    st.pyplot()
+
+#Sentiment Analysis
+
+
+
+
 
 st.sidebar.title('Product Review Analysis')
 option = st.sidebar.selectbox('Select one', ['Analysis', 'Input Review'])
@@ -33,6 +58,7 @@ if option == "Analysis":
         eda()
     else:
         st.title('Sentiment Analysis')
+        sentiment_analysis()
 
 else:
     st.title('Input Review')
